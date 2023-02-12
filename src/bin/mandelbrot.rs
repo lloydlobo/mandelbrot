6fecc5345166cb8c9eba1fec14d270345ac6d323
Lloyd Lobo <lloydlobo@gmail.com>
2023-02-12 15:28:04 +0530

    feat: lazily load config builder with once_cell

--- src/bin/mandelbrot.rs
+++ src/bin/mandelbrot.rs
@@ -1,17 +1,33 @@
-use std::{env};
+use std::{collections::HashMap, env, path::Path, sync::RwLock};
 
 use clap::{command, Arg, ArgMatches};
-use config::Config;
+use config::{Config, ConfigError};
 use console::Style;
 use indicatif::{ProgressBar, ProgressStyle};
 use log::info;
+use once_cell::sync::Lazy;
 use pretty_env_logger::env_logger::Builder;
 
 const ITERATIONS: u32 = 255;
 const WIDTH: u32 = 800;
 const HEIGHT: u32 = 800;
 const DEFAULT_SETTINGS_FILE: &str = "settings.toml";
-const PATH: &str = "mandelbrot.png";
+const DEFAULT_IMAGE_PATH: &str = "mandelbrot.png";
+
+static GLOBAL_SETTINGS: Lazy<RwLock<Config>> = Lazy::new(|| {
+    let mut curr_path = env::current_dir().unwrap();
+    curr_path.push(DEFAULT_SETTINGS_FILE);
+    RwLock::new(
+        // Add in `./settings.toml`
+        // Add in settings from the environment (with a prefix of APP)
+        // Eg.. `APP_DEBUG=1 ./target/app` would set the `debug` key
+        Config::builder()
+            .add_source(config::File::with_name(&curr_path.to_string_lossy()))
+            .add_source(config::Environment::with_prefix("APP"))
+            .build()
+            .unwrap(),
+    )
+});
 
 fn main() {
     Builder::from_default_env().format_timestamp(None).filter_level(log::LevelFilter::Info).init();
@@ -23,8 +39,9 @@ fn main() {
 }
 
 fn try_main() -> anyhow::Result<()> {
-    let settings = build_config_settings(DEFAULT_SETTINGS_FILE)?;
-    println!("{:?}", settings);
+    let settings: Config = GLOBAL_SETTINGS.read().unwrap().clone();
+    let settings: HashMap<String, String> = settings.try_deserialize()?;
+    println!("{settings:?}");
     let matches: ArgMatches = command!()
         .arg(
             Arg::new("ascii")
@@ -75,7 +92,7 @@ fn try_main() -> anyhow::Result<()> {
         );
         let pb = ProgressBar::new(WIDTH as u64 * HEIGHT as u64);
         style_progress_bar(&pb);
-        mandelbrot::mandelbrot_img::compose(WIDTH, HEIGHT, ITERATIONS).save(PATH)?;
+        mandelbrot::mandelbrot_img::compose(WIDTH, HEIGHT, ITERATIONS).save(DEFAULT_IMAGE_PATH)?;
         pb.finish_with_message("Saved image to file");
     }
 
@@ -91,14 +108,38 @@ fn style_progress_bar(pb: &ProgressBar) {
     );
 }
 
-// #[derive(Debug, Serialize,Deserialize)]
-// pub struct ConfigManger(config::Config);
-fn build_config_settings(path: &str) -> Result<Config, config::ConfigError> {
+/// `build_config_settings` is a wrapper around `config` crate.
+///
+/// # Examples
+///
+/// ```rust,ignore
+/// use config::Config;
+/// use mandelbrot::build_config_settings;
+///
+/// fn main() {
+///     // TODO: Add `tempfile` crate or `include_str!()`.
+///     let settings = build_config_settings("settings.toml")?;
+///     let map = settings.try_deserialize::<HashMap<String, String>>().unwrap();
+///     let expect = r#"{"key": "189rjfadoisfj8923fjio", "debug": "false", "priority": "32"}"#;
+///     assert_eq!(map, expect);
+/// }
+/// ```
+/// # Panics
+///
+/// Panics if .
+///
+/// # Errors
+///
+/// This function will return an error if .
+pub fn build_config_settings(path: &str) -> Result<Config, config::ConfigError> {
     let mut curr_path = env::current_dir().unwrap();
     curr_path.push(path);
+
     Config::builder()
         // Add in `./settings.toml`
         .add_source(config::File::with_name(&curr_path.to_string_lossy()))
+        // Add in settings from the environment (with a prefix of APP)
+        // Eg.. `APP_DEBUG=1 ./target/app` would set the `debug` key
         .add_source(config::Environment::with_prefix("APP"))
         .build()
 }
