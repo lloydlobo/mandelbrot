0fbb5244e1ceeae1939e7de0a0a6bce5519f1d93
Lloyd Lobo <lloydlobo@gmail.com>
2023-02-12 16:24:28 +0530

    feat: use paths in settings for output

--- src/bin/mandelbrot.rs
+++ src/bin/mandelbrot.rs
@@ -1,18 +1,21 @@
-use std::{collections::HashMap, env, sync::RwLock};
+use std::{env, path::Path, sync::RwLock};
 
 use clap::{command, Arg, ArgMatches};
 use config::{builder::DefaultState, Config, ConfigBuilder};
 use console::Style;
 use indicatif::{ProgressBar, ProgressStyle};
 use log::{info, LevelFilter::Info};
+use mandelbrot::mandelbrot_ascii;
 use once_cell::sync::Lazy;
 use pretty_env_logger::env_logger::Builder;
+use serde::{Deserialize, Serialize};
 
 const ITERATIONS: u32 = 255;
 const WIDTH: u32 = 800;
 const HEIGHT: u32 = 800;
 const DEFAULT_SETTINGS_FILE: &str = "settings.toml";
 const DEFAULT_IMAGE_PATH: &str = "mandelbrot.png";
+const DEFAULT_TEXT_PATH: &str = "mandelbrot.txt";
 
 static CONFIG_BUILDER: Lazy<RwLock<ConfigBuilder<DefaultState>>> =
     Lazy::new(|| RwLock::new(Config::builder()));
@@ -29,18 +32,15 @@ fn main() {
 fn try_main() -> anyhow::Result<()> {
     let mut curr_path = env::current_dir().unwrap();
     curr_path.push(DEFAULT_SETTINGS_FILE);
-    // TODO: Directly mutate `CONFIG_BUILDER` without assigning it.
-    let settings_builder: ConfigBuilder<DefaultState> = CONFIG_BUILDER
+    let settings_new: Config = CONFIG_BUILDER
         .write()
         .unwrap()
         .clone()
         .set_default("verbose", "1")? // This is not in the settings file.
         .add_source(config::File::with_name(&curr_path.to_string_lossy()))
-        .add_source(config::Environment::with_prefix("APP"));
-    // Does not take ownership of `ConfigBuilder` to allow later reuse.
-    let settings_new: Config = settings_builder.build_cloned()?;
-    // {"key": "189rjfadoisfj8923fjio", "verbose": "1", "priority": "32", "debug": "false"}
-    let _map_new = settings_new.try_deserialize::<HashMap<String, String>>()?;
+        .add_source(config::Environment::with_prefix("APP"))
+        .build_cloned()?;
+    let config_manager: ConfigManager = settings_new.try_deserialize()?;
 
     // Parse clap args.
     let matches: ArgMatches = command!()
@@ -69,9 +69,9 @@ fn try_main() -> anyhow::Result<()> {
         info!("Rendering image Mandelbrot set as {}", Style::new().bold().apply_to("ASCII"));
         let pb = ProgressBar::new(WIDTH as u64 * HEIGHT as u64);
         style_progress_bar(&pb);
-        let image = mandelbrot::mandelbrot_ascii::collect_ascii();
+        let image = mandelbrot_ascii::collect_ascii();
         pb.finish();
-        mandelbrot::mandelbrot_ascii::print_ascii(image);
+        mandelbrot_ascii::print_ascii(image);
     }
 
     if let Some(_text) = matches.get_one::<String>("text") {
@@ -81,8 +81,8 @@ fn try_main() -> anyhow::Result<()> {
         );
         let pb = ProgressBar::new(WIDTH as u64 * HEIGHT as u64);
         style_progress_bar(&pb);
-        let image = mandelbrot::mandelbrot_ascii::collect_ascii();
-        mandelbrot::mandelbrot_ascii::write_ascii_to_file(image);
+        let image = mandelbrot_ascii::collect_ascii();
+        mandelbrot_ascii::write_ascii_to_file(image, Path::new(&config_manager.text));
         pb.finish_with_message("Wrote ascii to file");
     }
 
@@ -93,7 +93,13 @@ fn try_main() -> anyhow::Result<()> {
         );
         let pb = ProgressBar::new(WIDTH as u64 * HEIGHT as u64);
         style_progress_bar(&pb);
-        mandelbrot::mandelbrot_img::compose(WIDTH, HEIGHT, ITERATIONS).save(DEFAULT_IMAGE_PATH)?;
+        if config_manager.image.is_empty() {
+            mandelbrot::mandelbrot_img::compose(WIDTH, HEIGHT, ITERATIONS)
+                .save(DEFAULT_IMAGE_PATH)?;
+        } else {
+            mandelbrot::mandelbrot_img::compose(WIDTH, HEIGHT, ITERATIONS)
+                .save(config_manager.image)?;
+        }
         pb.finish_with_message("Saved image to file");
     }
 
@@ -111,36 +117,36 @@ fn style_progress_bar(pb: &ProgressBar) {
 
 /// `build_config_settings` is a wrapper around `config` crate.
 ///
-/// # Examples
-///
-/// ```rust,ignore
-/// use config::Config;
-/// use mandelbrot::build_config_settings;
-///
-/// fn main() {
-///     // TODO: Add `tempfile` crate or `include_str!()`.
-///     let settings = build_config_settings("settings.toml")?;
-///     let map = settings.try_deserialize::<HashMap<String, String>>().unwrap();
-///     let expect = r#"{"key": "189rjfadoisfj8923fjio", "debug": "false", "priority": "32"}"#;
-///     assert_eq!(map, expect);
-/// }
-/// ```
-/// # Panics
-///
-/// Panics if .
-///
-/// # Errors
-///
-/// This function will return an error if .
+/// * `build_cloned` method - Does not take ownership of `ConfigBuilder` to allow later reuse.
+/// let map_new = settings_new.try_deserialize::<HashMap<String, String>>()?;
+// NOTE: Directly mutate `CONFIG_BUILDER` without assigning it.
 pub fn build_config_settings(path: &str) -> Result<Config, config::ConfigError> {
     let mut curr_path = env::current_dir().unwrap();
     curr_path.push(path);
 
+    // Add in `./settings.toml`
+    // Add in settings from the environment (with a prefix of APP)
+    // Eg.. `APP_DEBUG=1 ./target/app` would set the `debug` key
     Config::builder()
-        // Add in `./settings.toml`
         .add_source(config::File::with_name(&curr_path.to_string_lossy()))
-        // Add in settings from the environment (with a prefix of APP)
-        // Eg.. `APP_DEBUG=1 ./target/app` would set the `debug` key
         .add_source(config::Environment::with_prefix("APP"))
         .build()
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct ConfigManager {
+    text: String,
+    image: String,
+}
+
+impl Default for ConfigManager {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl ConfigManager {
+    fn new() -> Self {
+        Self { text: DEFAULT_TEXT_PATH.to_string(), image: DEFAULT_IMAGE_PATH.to_string() }
+    }
 }
